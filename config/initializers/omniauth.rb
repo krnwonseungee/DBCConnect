f8128631e33f7b4dc969fff724f4f4766cff7850
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-04-11 13:33:52 -0700

    Oauth initializer rb file
    * sets up a provider for linkedin
    * other providers could be added here

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
+  puts "linkedin_secret is: #{ENV['LINKEDIN_SECRET']}"
 end
