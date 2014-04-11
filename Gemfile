b97e9f9b3b62aa0daed821fddf7b5f675787ce6e
Eric <ericjosjohnson@gmail.com>
2014-04-11 14:01:27 -0700

    Remove references to HTTParty, clean up .gitignore

--- Gemfile
+++ Gemfile
@@ -30,7 +30,6 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
-gem "httparty"
 gem 'dbc-ruby'
 
 group :doc do
--- config/initializers/dbc_api.rb
+++ config/initializers/dbc_api.rb
@@ -1,8 +1,6 @@
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
-# Twitter
+
 env_config = YAML.load_file(APP_ROOT.join('dbc_api_key.yml'))
 
 ENV['DBC_API'] = env_config['DBC-API']
 
-# HTTParty.get('http://example.com', :headers => {"User-Agent" => APPLICATION_NAME})
-# HTTParty.get("https://api.devbootcamp.com/v1/users/869", :headers => {DBC-API: "3d81ab0f46b40ea0546b1d64976180df"})
