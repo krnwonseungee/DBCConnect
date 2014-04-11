684e8107e4258bb845b0a4bcea1031d427ab577c
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-04-11 11:22:56 -0700

    Taking out some 'puts' I accidently left in

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -2,8 +2,6 @@ module DBCconnect
   class Application < Rails::Application
     config.before_configuration do
       env_file = Rails.root.join("config", 'environment_variables.yml').to_s
-      puts ("trying to add #{Rails.root.join("config", 'environment_variables.yml').to_s}")
-      puts ("file is: env_file: #{env_file}")
       if File.exists?(env_file)
         #Note: original snippet for line below had "[Rails.env]" before each do
         #Couldn't make it work right, so removed.  [Rails.env] = ["development"]
