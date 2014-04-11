593f3f61dc0e62ade4da98fc89e35bd65a081cfd
Eric <ericjosjohnson@gmail.com>
2014-04-10 21:41:34 -0700

    DevBootcamp API setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+all_boots = DBC::User.all
+all_cohorts = DBC::Cohort.all
