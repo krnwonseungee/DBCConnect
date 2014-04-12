3b5c2ac90828d113cf3c642c3940353b58badf94
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-04-11 17:43:15 -0700

    removed an annoying 'puts' statement

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
-  puts "linkedin_secret is: #{ENV['LINKEDIN_SECRET']}"
 end
