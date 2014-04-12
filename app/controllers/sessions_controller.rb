40710178fb40636656f8d6a8d1a875582cc3b72f
Apprentice <apprentice@dbc28.local>
2014-04-12 12:58:56 -0700

    Implemented sessions, user lookup from db based on linkedin auth_hash
    * user_id is stored in session[:user_id]
    * if lookup fails, session[:lookup_error] is set to true
    * time to add unit/controller tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,17 @@
 class SessionsController < ApplicationController
   def create
-    puts "in sessions create, auth_hash is:"
-    puts "#{auth_hash}"
-    # Need to actually create a user from the auth_hash
-    # Auth_hash contains a bunch of public-level user data of whoever logged in
-    # So eventually, something like:
-    # @user = User.find_or_create_from_auth_hash(auth_hash)
-    # self.current_user = @user
-    redirect_to '/'
+    if @user = User.lookup_by_auth_hash(auth_hash)
+      session[:user_id] = @user.id
+      session[:lookup_error] = false
+    else
+      session[:user_id] = nil
+      session[:lookup_error] = true
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+    logout
   end
 
   protected
@@ -17,7 +21,25 @@ class SessionsController < ApplicationController
   end
 
   def logout
-    session.clear
+    reset_session
     redirect_to root_path
   end
 end
+
+
+
+# CAN DELETE COMMENTS BELOW IF DESIRED!
+# It's not code, never used, just a console log of what auth_hash is returning from omniauth_linkedin request:
+
+# auth_hash (e.g. request.env['omniauth.auth']) is:
+
+# #<OmniAuth::AuthHash
+# credentials=#<OmniAuth::AuthHash expires=true expires_at=1402511642 token="AQXc3JwUR9CQw93DXCTDE1fDt1et-eJYLah4-deT4gnHXk0hmNz9y0VAnnGqgKLuaV-rabXt23gDnCfhNsTuSlVvbvpBOgC7cgBRGMEtOn57vs8mMFEbzlyU6eDxvf1wMnEDyHPSAXFVustTp6tuzq9YLRv1LDdStI-6MRpcjJIQgtoETiA">
+
+# extra=#<OmniAuth::AuthHash
+
+# raw_info=#<OmniAuth::AuthHash emailAddress="Matt.b.bechtel@gmail.com" firstName="Matt" headline="Staff Engineer at Northgate Environmental Management" id="KqO9Xhy3cn" industry="Environmental Services" lastName="Bechtel" location=#<OmniAuth::AuthHash country=#<OmniAuth::AuthHash code="us"> name="San Francisco Bay Area"> publicProfileUrl="http://www.linkedin.com/in/bechtelm">>
+
+# info=#<OmniAuth::AuthHash::InfoHash description="Staff Engineer at Northgate Environmental Management" email="Matt.b.bechtel@gmail.com" first_name="Matt" image=nil last_name="Bechtel" location=#<OmniAuth::AuthHash country=#<OmniAuth::AuthHash code="us"> name="San Francisco Bay Area"> name="Matt Bechtel" nickname="Matt Bechtel"
+
+# urls=#<OmniAuth::AuthHash public_profile="http://www.linkedin.com/in/bechtelm">> provider="linkedin" uid="KqO9Xhy3cn">
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ActiveRecord::Base
   belongs_to :cohort
   has_many :requestors
   has_many :responders
+
+  def self.lookup_by_auth_hash(auth_hash)
+    user = User.find_by_linked_in(auth_hash.info.urls.public_profile)
+    #Can eventually add in secondary checks by name/gmail etc if no linkedin url on socrates
+    return user || false
+  end
+
   include PgSearch
   multisearchable :against => [ :name,
                                 :email,
@@ -23,4 +30,5 @@ class User < ActiveRecord::Base
                                 :location
                               ]
 end
+
 
