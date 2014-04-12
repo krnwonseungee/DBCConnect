b53bc3d0933b6ffea24d52da1a450bada88cb7b5
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-04-11 19:02:18 -0700

    Enables Awesome Font (customizable icons) for avail/unavail status on sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 DBCconnect::Application.routes.draw do
   root 'users#index'
+  #test route for sidebar_avail_users
+  get 'users/sidebar_avail', to: 'users#sidebar_avail', as: 'sidebar_avail'
+
   resources :users do
     resources :requestors
     resources :responders
   end
 
   resources :cohorts, only: [:index, :show]
+
+
 end
