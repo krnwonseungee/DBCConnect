e2ae5c0e00b1db41119aace3ad5004f2f798808b
Katherine Hayden <katherineimogene@gmail.com>
2014-04-12 16:54:43 -0700

    User model checks for rails environment before geocoding   * will not run in test environment.

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,8 @@ class User < ActiveRecord::Base
                                 :location
                               ]
   geocoded_by :current_location
-  after_validation :geocode
+  unless Rails.env.test?
+    after_validation :geocode
+  end
 end
 
