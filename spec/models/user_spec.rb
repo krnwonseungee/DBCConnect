d6446b21e84c0899ca91c1a48261a28c3fcb50d0
Apprentice <apprentice@dbc28.local>
2014-04-12 16:43:15 -0700

    Tests added for user mode "lookup_from_auth_hash" method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,20 @@
 require 'spec_helper'
 
 describe User do
+
   context "associations" do
     it { should have_many(:requestors) }
     it { should have_many(:responders) }
     it { should belong_to(:cohort) }
   end
 
-  context "matching oauth return to db" do
-    good_url  = "www.validurl.edu"
+  context "looking up oauth response in db" do
+    user = FactoryGirl.create(:user, first_name: "Joe")
+    good_url  = user.linked_in
     bad_url = "www.invalidurl.me"
-    before(:each) do
-      target_user = User.create(name: "Charles Dickens", linked_in: good_url)
-      puts "CREATED A USER FOR TEST"
-      p target_user
-    end
-
-    after(:each) do
-      User.last.destroy
-    end
 
     it "returns the correct user when given valid input" do
-      puts "IN FIRST TEST"
-      puts "lookup running."
-      expect(User.lookup_from_auth_hash({linkedin_url: good_url})).to eq target_user
+      expect(User.lookup_from_auth_hash({linkedin_url: good_url})).to eq user
     end
     it "returns false when there's no lookup match" do
       expect(User.lookup_from_auth_hash(linkedin_url: bad_url)).to be false
