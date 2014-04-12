7946cd2bfebc3cb2b6b9de256788775a863997b3
Apprentice <apprentice@dbc28.local>
2014-04-12 13:35:27 -0700

    Starting on user specs relating to login

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,12 @@ describe User do
     it { should have_many(:responders) }
     it { should belong_to(:cohort) }
   end
+
+  context "looking up db values using omniauth_hash values" do
+    it "returns the correct user when given valid input" do
+    end
+    it "returns false when there's no lookup match" do
+    end
+  end
+
 end
