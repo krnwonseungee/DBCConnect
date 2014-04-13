5300a8daa18a1e3211c85479fe4ce55c9f8c0692
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-04-13 13:27:25 -0700

    Adding name check if linkedin email lookup fails

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ActiveRecord::Base
   has_many :responders
 
   def self.lookup_from_auth_hash(opts = {})
-    user = User.find_by_linked_in(opts[:linkedin_url])
+    if user = User.find_by_linked_in(opts[:linkedin_url])
+      return user
+    elsif
+      user = User.find_by_name(opts[:name])
+      return user
+    else
     #Can eventually add in secondary checks by name/gmail etc if no linkedin url on socrates
-    return user || false
+      return false
+    end
   end
 
   include PgSearch
