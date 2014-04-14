023d18cc515db51c0e14065bb82d24b90f46b61e
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-04-13 21:25:41 -0700

    Adds bindEvents function to search-results-controller

--- app/assets/javascripts/search-results-controller.js
+++ app/assets/javascripts/search-results-controller.js
@@ -1,13 +1,25 @@
 SearchResults.Controller = function(){}
 
 SearchResults.Controller.prototype = {
+  bindEvents: $("#searchbar").submit(function(e){
+    e.preventDefault();
+    // view.showResults();
+    console.log("poo")
+  },
+
   retrieveResults: function(){
     $.ajax({
       type: "get",
-      url: "/users/results"
-      dataType: "json"
+      url: "/users/results",
+      dataType: "json",
     }).done(function(serverData{
-      searchResults.list = serverData.user_obj_array
+      console.log("poo")
+      // searchResults.list = serverData.user_obj_array
     }))
   }
 }
+
+$(document).ready(function(){
+  controller = new SearchResults.Controller
+  controller.bindEvents
+})
