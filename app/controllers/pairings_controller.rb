b8e0416972ec5c771aa4c5299883ce310022e2d6
Zohar Liran <zoharliran@gmail.com>
2014-04-14 11:10:21 -0700

    removed cors

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -1,17 +1,5 @@
 class PairingsController < ApplicationController
   before_action :set_pairing, only: [:show, :edit, :update, :destroy]
-  before_filter: allow_cors
-
-  def allow_cors
-    headers["Access-Control-Allow-Origin"] = "*"
-    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
-    headers["Access-Control-Allow-Headers"] =
-      %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
-
-    head(:ok) if request.request_method == "OPTIONS"
-    # or, render text: ''
-    # if that's more your style
-  end
   
   def index
     @pairings = Pairing.where("requestor_id = ? OR responder_id = ?",current_user.id,current_user.id)
