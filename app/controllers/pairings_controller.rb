819ef0d9219d39f0f89c3c6cff4a542f154e2db3
Zohar Liran <zoharliran@gmail.com>
2014-04-14 12:36:24 -0700

    final routes with OPTION and header in put route

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -1,13 +1,11 @@
 class PairingsController < ApplicationController
   before_action :set_pairing, only: [:show, :edit, :update, :destroy]
   skip_before_filter :verify_authenticity_token
-  
-  def allow_cors
-    headers["Access-Control-Allow-Origin"] = "*"
-    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
-    headers["Access-Control-Allow-Headers"] =
-      %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
 
+  
+
+  def allow_cors
+    set_headers
     head(:ok) if request.request_method == "OPTIONS"
     # or, render text: ''
     # if that's more your style
@@ -54,7 +52,7 @@ class PairingsController < ApplicationController
 
   #The route waits for a put request created by the hangout app gadget
   def update_hangout_info
-    render text: "THE OTHER ROUTE" and return
+    set_headers
     # Down the road, should have a better way of finding the correct pair
     # Could break if there's more than one request e.g. returning one pair's link to a different pair
     pairing = Pairing.last
@@ -63,6 +61,13 @@ class PairingsController < ApplicationController
   end
 
   private
+    def set_headers
+      headers["Access-Control-Allow-Origin"] = "*"
+      headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+      headers["Access-Control-Allow-Headers"] =
+        %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+    end
+
     def set_pairing
       @pairing = Pairing.find(params[:id])
     end
