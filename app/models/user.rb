17c7bd9c6243cb18014b3b04ad508d1d70d157f7
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-04-15 15:44:56 -0700

    now handles both /pub and /in linkedin urls.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,18 @@ class User < ActiveRecord::Base
     # e.g. 'http://www.linkedin.com/in/bechtelm' becomes 'bechtelm'
     li_url_substring_in = opts[:linkedin_url][/(?<=in\/)[\w-]+/]
     li_url_substring_pub = opts[:linkedin_url][/(?<=pub\/)[\w-]+/]
-    if li_url_substring_in || li_url_substring_pub #don't want to waste time if match was nil due to other url format
+    puts "$" *200
+    puts "strings are:"
+    p li_url_substring_in, li_url_substring_pub
+    if li_url_substring_in  #don't want to waste time if match was nil due to other url format
       user = User.find(:all, :conditions => ["linked_in LIKE ?", "%#{li_url_substring_in}%"])
-      # user ||= User.find(:all, :conditions =>["linked_in LIKE ?", "%#{li_url_substring_pub}%"])
-      user = user[0] if user #grabs result out of array, only if there was a result (avoids nil[0] -> error)
+    elsif li_url_substring_pub
+      user ||= User.find(:all, :conditions =>["linked_in LIKE ?", "%#{li_url_substring_pub}%"])
     end
+    user = user[0] if user #grabs result out of array, only if there was a result (avoids nil[0] -> error)
     user ||= User.find_by_name(opts[:name])
+    puts "THE USER IS:"
+    p user
   end
 
   include PgSearch
