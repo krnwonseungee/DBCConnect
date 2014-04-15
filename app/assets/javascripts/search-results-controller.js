37ca91ad7cfed598f23cd0810324b6272c12c22c
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-04-15 10:09:56 -0700

    Removes comments and console.log in search-results-controller.js

--- app/assets/javascripts/search-results-controller.js
+++ app/assets/javascripts/search-results-controller.js
@@ -5,50 +5,32 @@ SearchResults.Controller.prototype = {
     $("#searchbar").submit(function(e){
       e.preventDefault();
       var searchValue = $("input:first").val();
-      // console.log("SEARCHVALUE" + searchValue)
       searchResultsController.retrieveResults(searchValue);
     })
 
     $('#main').on("click", ".profilelink", function(e){
       e.preventDefault();
-      console.log('poo');
       var clickedLinkPathname = $(this).attr('href')
       var userId = clickedLinkPathname.substr(clickedLinkPathname.length - 3)
-      console.log(userId);
       searchResultsController.fetchUserInfo(userId);
     })
 
     $('#main').on("click", ".editproflink", function(e){
       e.preventDefault();
       var editLinkPathname = $(this).attr('href')
-      console.log(editLinkPathname);
-      // debugger
-      // var editUserId = editLinkPathname.substr(editLinkPathname.length - 8)
-      // console.log(editUserId);
       searchResultsController.fetchEditUserInfo(editLinkPathname);
     })
 
     $('#main').on("submit", ".update_user", function(e){
       e.preventDefault();
       rerouteProfLink = this.action
-      // var clickedLinkPathname = $(this).attr('href')
       var rerouteUserId = rerouteProfLink.substr(rerouteProfLink.length - 3)
-      // console.log(userId);
       searchResultsController.fetchUserInfo(rerouteUserId);
     })
-
-    // $("#searchbar").submit(function(e){
-    //   e.preventDefault();
-    //   var searchValue = $("input:first").val();
-    //   // console.log("SEARCHVALUE" + searchValue)
-    //   searchResultsController.retrieveResults(searchValue);
-    // })
-
   },
 
 
   retrieveResults: function(searchValue){
-      // debugger
     $.ajax({
       type: "post",
       url: "/users/results",
