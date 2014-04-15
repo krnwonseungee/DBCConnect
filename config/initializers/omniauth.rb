589551e251841f7cb39b3317a1f8356867192e4c
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-04-15 10:13:00 -0700

    Adding comment in omniauth initializer about default linkedin fields

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
+  #Default fields grabbed by omniauth-linkedin:
+  # ['id', 'email-address', 'first-name', 'last-name', 'headline', 'location', 'industry', 'picture-url', 'public-profile-url']
+
 end
