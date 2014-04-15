39db9b7f08db1f54e0dcadab6f28ba6943884d33
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-04-15 15:28:02 -0700

    handling additional linkedin link formats

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ActiveRecord::Base
   def self.lookup_from_auth_hash(opts = {})
     #Regex in next line finds "in/" then grabs the user public profile id after
     # e.g. 'http://www.linkedin.com/in/bechtelm' becomes 'bechtelm'
-    linkedin_url_substring = opts[:linkedin_url][/(?<=in\/)\w+/]
-    if linkedin_url_substring #don't want to waste time if match was nil due to other url format
-      user = User.find(:all, :conditions => ["linked_in LIKE ?", "%#{linkedin_url_substring}%"])
+    li_url_substring_in = opts[:linkedin_url][/(?<=in\/)[\w-]+/]
+    li_url_substring_pub = opts[:linkedin_url][/(?<=pub\/)[\w-]+/]
+    if li_url_substring_in || li_url_substring_pub #don't want to waste time if match was nil due to other url format
+      user = User.find(:all, :conditions => ["linked_in LIKE ?", "%#{li_url_substring_in}%"])
+      # user ||= User.find(:all, :conditions =>["linked_in LIKE ?", "%#{li_url_substring_pub}%"])
       user = user[0] if user #grabs result out of array, only if there was a result (avoids nil[0] -> error)
     end
     user ||= User.find_by_name(opts[:name])
