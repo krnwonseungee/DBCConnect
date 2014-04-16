8d2936cbc367624c9835c5da07c22bc6b296939e
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-04-15 18:52:19 -0700

    Includes hardcoded quotes in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,25 @@
 
 all_boots = DBC::User.all
 all_cohorts = DBC::Cohort.all
+all_quotes = Quote.all
+
+quote_author_arr = [
+    "Confusion is the feeling that comes before understanding.",
+    "The middle of every successful project looks like a disaster.",
+    "Luck is when preparedness meets opportunity, and opportunity is everywhere.",
+    "Make no little plans; they have no magic to stir men's blood.",
+    %q{ The indispensable first step to getting the things you want out of
+        life is this: decide what you want. },
+    "Opportunity dances with those who are already on the dance floor."
+]
+quote_content_arr = [
+    "Shereef Bishay",
+    "Rosabeth Moss Cantor",
+    "Earl Nightingale",
+    "Daniel Burnham",
+    "Ben Stein",
+    "H. Jackson Brown Jr."
+]
 
 all_cohorts.each do |cohort|
   c = Cohort.create(
@@ -29,5 +48,12 @@ all_boots.each do |boot|
     about: boot.profile[:about],
     hometown: boot.profile[:hometown],
     current_location: boot.profile[:current_location]
+    )
+end
+
+all_quotes.each_with_index do |i|
+    quote = Quote.create(
+        content: quote_author_arr[i],
+        author: quote_content_arr[i]
     )
 end
