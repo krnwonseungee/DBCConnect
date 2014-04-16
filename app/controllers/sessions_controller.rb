b4897fca7458c9ce173d88a75fabc5f95de29a74
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-04-15 19:00:05 -0700

    Call update from linked in method if omniauth login works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,8 @@
 class SessionsController < ApplicationController
   def create
-    puts "$" * 300
-    puts "auth hash is:"
-    p auth_hash
-    puts "auth_hash.extra.raw_info.positions.values is:"
-    p auth_hash.extra.raw_info.positions.values
-    puts "auth_hash.extra.raw_info.positions.values.to_hash is:"
-    p auth_hash.extra.raw_info.positions.values.to_hash
-    puts "$" * 300
     if user = User.lookup_from_auth_hash(auth_hash)
       session[:user_id] = user.id
+      user.update_records_from_linkedin_auth_hash(auth_hash)
       redirect_to welcome_path
     else
       flash[:notice] = "DBC Grad Lookup failed. Linkedin account url or name does not match our records."
