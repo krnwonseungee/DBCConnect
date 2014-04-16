ebdcb58bfe6c37ad7dba2f1282f3e1ffdbd46981
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-04-16 10:26:41 -0700

    Removes commented out results route in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,26 +39,7 @@ class UsersController < ApplicationController
     user = current_user
   end
 
-  # def results #refactor to be skinner model
-  #   user_content_array = Array.new
-  #   pg_search_results = PgSearch.multisearch(params[:pgsearch]).each do |result|
-  #     user_content_array.push(result.content)
-  #   end
-
-  #   resulting_names = Array.new
-  #   user_content_array.each do |user_content|
-  #     user_names = user_content.split(" ").take(2).join(" ")
-  #     resulting_names.push(user_names)
-  #   end
-  #   @user_obj_array = Array.new
-  #   resulting_names.each do |name|
-  #     @user_obj_array << User.find_by_name(name)
-  #   end
-
-  #   render partial: 'results', locals: { results: @user_obj_array }
-  # end
-
-  def results #refactor to be skinner model
+  def results
     @user_obj_array = User.get_list_of_user_obj(params[:pgsearch])
     render partial: 'results', locals: { results: @user_obj_array }
   end
