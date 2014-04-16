bd6be6d90e9d53cfd9230c6ff092500851e21a3f
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-04-16 10:13:53 -0700

    Moves method in user#results (controller) to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,6 +60,25 @@ class User < ActiveRecord::Base
       :if => lambda{ |user| user.current_location_changed? }
   end
 
+  def self.get_list_of_user_obj(search_term)
+    user_content_array = Array.new
+    pg_search_results = PgSearch.multisearch(search_term).each do |result|
+      user_content_array.push(result.content)
+    end
+
+    resulting_names = Array.new
+    user_content_array.each do |user_content|
+      user_names = user_content.split(" ").take(2).join(" ")
+      resulting_names.push(user_names)
+    end
+    user_obj_array = Array.new
+    resulting_names.each do |name|
+      user_obj_array << User.find_by_name(name)
+    end
+
+    user_obj_array
+  end
+
   private
 
   def user_params
