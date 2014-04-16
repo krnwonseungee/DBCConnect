2b6fdcb02e8b2e0108860ed6f74119b79c4bb0f4
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-04-16 10:20:48 -0700

    Refactors get_list_of_user_obj method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,19 +61,11 @@ class User < ActiveRecord::Base
   end
 
   def self.get_list_of_user_obj(search_term)
-    user_content_array = Array.new
-    pg_search_results = PgSearch.multisearch(search_term).each do |result|
-      user_content_array.push(result.content)
-    end
-
-    resulting_names = Array.new
-    user_content_array.each do |user_content|
-      user_names = user_content.split(" ").take(2).join(" ")
-      resulting_names.push(user_names)
-    end
     user_obj_array = Array.new
-    resulting_names.each do |name|
-      user_obj_array << User.find_by_name(name)
+    pg_search_results = PgSearch.multisearch(search_term).each do |result|
+      user_name = result.content.split(" ").take(2).join(" ")
+      user_obj = User.find_by_name(user_name)
+      user_obj_array << user_obj
     end
 
     user_obj_array
