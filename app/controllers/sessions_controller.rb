3b092e53bcc2594c647764f8146ed560854d6748
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-04-15 17:09:00 -0700

    added puts to help figure out 'positions' Mash from linkedin auth_hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
   def create
+    puts "$" * 300
+    puts "auth hash is:"
+    p auth_hash
+    puts "$" * 300
     if user = User.lookup_from_auth_hash(auth_hash)
       session[:user_id] = user.id
       redirect_to welcome_path
