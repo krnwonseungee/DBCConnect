437ca37b3a80e7a5c18402b80c14c0f817ef1417
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-04-16 19:56:20 -0700

    Moves PgSearch and multisearchable :against to the top in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,28 @@ class User < ActiveRecord::Base
   belongs_to :cohort
   has_many :requests
 
+  include PgSearch
+  multisearchable :against => [ :name,
+                                :email,
+                                :bio,
+                                :role,
+                                :github,
+                                :quora,
+                                :twitter,
+                                :facebook,
+                                :linked_in,
+                                :blog,
+                                :about,
+                                :hometown,
+                                :current_location,
+                                :first_name,
+                                :last_name,
+                                :position,
+                                :company,
+                                :location
+                              ]
+  geocoded_by :current_location
+
   def self.lookup_from_auth_hash(auth_hash)
     opts = {}
     opts[:linkedin_url] = auth_hash.info.urls.public_profile
@@ -29,27 +51,6 @@ class User < ActiveRecord::Base
     end
   end
 
-  include PgSearch
-  multisearchable :against => [ :name,
-                                :email,
-                                :bio,
-                                :role,
-                                :github,
-                                :quora,
-                                :twitter,
-                                :facebook,
-                                :linked_in,
-                                :blog,
-                                :about,
-                                :hometown,
-                                :current_location,
-                                :first_name,
-                                :last_name,
-                                :position,
-                                :company,
-                                :location
-                              ]
-  geocoded_by :current_location
   unless Rails.env.test?
     after_validation :geocode,
       :if => lambda{ |user| user.current_location_changed? }
