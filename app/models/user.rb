c905ef9720a24953a8194f27f1d256e01f998d81
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-04-16 18:34:27 -0700

    removing dead code from user that was copied/moved to linkedinInterface

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,6 @@ class User < ActiveRecord::Base
   belongs_to :cohort
   has_many :requests
 
-  def self.lookup_from_auth_hash(auth_hash)
-    UserLinkedinInterface.user_lookup_by_linkedin_data(auth_hash)
-  end
-
-  def update_records_from_linkedin_auth_hash(auth_hash)
-    if pic_url = grab_picture_url_from_linkedin(auth_hash)
-      self.update_attribute(:picture_url, pic_url)
-    end
-    if company_names = grab_company_names_from_linkedin(auth_hash)
-      self.update_attribute(:company, company_names)
-    end
-    if location = grab_location_from_linkedin(auth_hash)
-      self.update_attribute(:location, location)
-    end
-  end
-
   include PgSearch
   multisearchable :against => [ :name,
                                 :email,
@@ -58,22 +42,6 @@ class User < ActiveRecord::Base
   private
   def user_params
     params.require(:user).permit(:current_location, :company, :github, :twitter, :facebook, :blog)
-  end
-
-  def grab_company_names_from_linkedin(auth_hash)
-    company_names = []
-    auth_hash.extra.raw_info.positions.values[1].each do |company_hash|
-      c_name = company_hash['company']['name']
-      company_names << c_name if c_name
-    end
-    company_names.join(", ")
-  end
-
-  def grab_picture_url_from_linkedin(auth_hash)
-    auth_hash.extra.raw_info.pictureUrl
-  end
-  def grab_location_from_linkedin(auth_hash)
-    auth_hash.extra.raw_info.location.name
   end
 
 end
