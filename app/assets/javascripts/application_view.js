6b9f5182de8ac2567bf12deef3538e4241b8fc3a
Steven G. Harms <steven@devbootcamp.com>
2014-04-24 14:44:01 -0700

    Remove renderPartial, renderMap, showNumOfCities, hidePartial
    These are some weird view manipulating functions
    whose behavior is probably better located
    elsewhere.  They're all pretty simple so I decided
    to axe them entirely.

--- app/assets/javascripts/application_view.js
+++ app/assets/javascripts/application_view.js
@@ -25,25 +25,5 @@ Application.View.prototype = {
     $('#pairing-link').on('click', function(){
       $('#close-pop-up').click()
     })
-  },
-
-  renderPartial: function(partial){
-    $("#logo-in-map").empty();
-    // this will bring a new quote and show the map again
-    $('#container').empty().html(partial)
-  },
-
-  renderMap: function(){
-    $('#container').empty().html("<div id='map'></div>");
-    controller.createMap();
-  },
-
-
-  showNumOfCities: function(cityCount){
-    $("#logo-in-map").append("<h2>DBC CONNECT</h2>Find your fellow boots in " + cityCount + " cities around the world")
-  },
-
-  hidePartial:function(e){
-    $(e.target).parent().parent().hide()
   }
 }
