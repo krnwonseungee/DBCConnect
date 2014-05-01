48ec5c283aca497bd67cddd5e0d74b7843ebf240
Steven G. Harms <steven@devbootcamp.com>
2014-05-01 09:53:29 -0700

    Handlebarsify: user_popup

--- app/assets/javascripts/boot_map/popup_presenter.js
+++ app/assets/javascripts/boot_map/popup_presenter.js
@@ -1,13 +1,12 @@
 BootMap.PopupPresenter = function(boot, opts) {
   this.boot = boot;
   if (!opts) opts = {};
-  this.templateSel = opts.templateSel || "#user-popup";
-  if (!BootMap.template) BootMap.template = Handlebars.compile($(this.templateSel).html());
+  this.templateSel = opts.templateSel || "user_popup";
 };
 
 BootMap.PopupPresenter.prototype = {
   present: function () {
-    return BootMap.template(this.boot);
+    return HandlebarsTemplates[this.templateSel](this.boot);
   }
 }
 
